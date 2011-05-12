0dc15cfe074f934885f1d0a506ff2af25844eca0
chandankmit <chandankmit@55a0e026-e783-40ef-8df5-1463b6136d98>
2011-05-12 12:48:12 +0000

    + code added for using a user-defined/particular schema to validate the pathway against it using a "choose schema" button. + And exporting the current active pathway in Pathvisio into ".mimml" file (mimml format) and using that file object to validate against the current selected schema. These steps are performed whenever the user presses the "validate" button

--- Pathway-Validator/trunk/myplugin/HelloPlugin.java
+++ Pathway-Validator/trunk/myplugin/HelloPlugin.java
@@ -8,15 +8,21 @@ import java.io.File;
 
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter.DEFAULT;
 
+import org.pathvisio.Engine;
 import org.pathvisio.gui.swing.PvDesktop;
+import org.pathvisio.gui.swing.SwingEngine;
+import org.pathvisio.model.ConverterException;
+import org.pathvisio.model.GpmlFormat;
 import org.pathvisio.plugin.Plugin;
 
 /**
@@ -25,10 +31,20 @@ import org.pathvisio.plugin.Plugin;
 public class HelloPlugin implements Plugin,ActionListener
 {
 	private PvDesktop desktop;
-	JButton valbutton;
-	JTextArea jta;
+	private JButton valbutton;
+	private JTextArea jta;
+	private File schemaFile=null;
+	private File currentPathwayFile;
+	
 	public void init(PvDesktop desktop) 
-	{   
+	{   currentPathwayFile=new File("C:\\Users\\kayne\\Desktop\\currentPathwaytmp.mimml");
+		//chandan : creating a button for choosing the schema file 
+		JButton chooseSchema=new JButton("choose Schema");
+		chooseSchema.setActionCommand("choose");
+		chooseSchema.addActionListener(this);
+		//chooseSchema.setHorizontalAlignment(JButton.RIGHT);
+	    //chandan
+		
 		// save the desktop reference so we can use it later
 		this.desktop = desktop;
 		
@@ -53,10 +69,12 @@ public class HelloPlugin implements Plugin,ActionListener
       /* for(int i=0;i<8;i++){
         jta.append("error @ GraphId : e02bf - An interaction should not start and end with Line arrowheads."+"\n");
         }*/
+ 
         valbutton=new JButton("validate");
         valbutton.setActionCommand("display");
         valbutton.addActionListener(this);
         mySideBarPanel.add(valbutton);
+        mySideBarPanel.add(chooseSchema);
         
         // get a reference to the sidebar
         JTabbedPane sidebarTabbedPane = desktop.getSideBarTabbedPane();
@@ -106,15 +124,50 @@ public class HelloPlugin implements Plugin,ActionListener
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		
 		if ("display".equals(e.getActionCommand())) {
 			
+			if(schemaFile==null){
+				JOptionPane.showMessageDialog(
+						desktop.getFrame(), 
+						"Please set the schema to use and then press validate");
+				return;
+			}
+			SwingEngine se=desktop.getSwingEngine();
+			Engine eng=se.getEngine();
+			if(eng.hasVPathway()){
+			
+			MIMFormat mimf=new MIMFormat();
+			try {
+				mimf.doExport(currentPathwayFile, eng.getActivePathway());
+			} catch (ConverterException e1) {
+				// TODO Auto-generated catch block
+				System.out.println("converter Exception");
+				e1.printStackTrace();
+			}
+				/*exporting the pathway in gpml format and then accesing the file object */
+			/*try {
+				GpmlFormat.writeToXml(eng.getActivePathway(), currentPathwayFile, false);
+			} catch (ConverterException e1) {
+				// TODO Auto-generated catch block
+				System.out.println("converter Exception");
+				e1.printStackTrace();
+			}*/
+				
+			}
+			else {
+				JOptionPane.showMessageDialog(
+						desktop.getFrame(), 
+						"There's no pathway open to validate");
+			return;
+			}
 			SchematronTask st=new SchematronTask();
-            st.setSchema(new File("D:\\schematron\\mimschema.sch"));
+            st.setSchema(schemaFile);
             st.setQueryLanguageBinding("xslt2");
             //st.setOutputEncoding(null);
             st.setOutputDir("C:\\Users\\kayne\\Desktop");
             st.setFormat("svrl");
-            st.setFile(new File("D:\\schematron\\new1.mimml"));
+            st.setFile(currentPathwayFile);
             //st.setClasspath(Path.systemClasspath);
             //st.setFileDirParameter(null);
             //st.setArchiveNameParameter(null);
@@ -123,14 +176,37 @@ public class HelloPlugin implements Plugin,ActionListener
             //st.setPhase(null);
             st.execute();
             jta.setText(null);
+            
             while (st.failed_itr.hasNext()) {
             	jta.append(st.failed_itr.next()+"\n");
-          }
-           //jta.setCaretPosition(jta.getDocument().getLength());
+            }
+            //jta.setCaretPosition(jta.getDocument().getLength());
 	        //System.out.println("Hello World!-"+Path.systemClasspath);	
             System.out.println("validate button pressed ");    
 		
-		} 
+		}
+		else{
+			
+			System.out.println("choose schema button pressed");
+			
+			JFileChooser chooser = new JFileChooser();
+		    chooser.setDialogTitle("schema to use");
+		    chooser.setApproveButtonText("use schema");
+			
+		    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+		        "schema files(.sch)", "sch");
+		    
+		    chooser.setFileFilter(filter);
+		  
+		    int returnVal = chooser.showOpenDialog(desktop.getFrame());
+		    
+		    if(returnVal == JFileChooser.APPROVE_OPTION) {
+		       System.out.println("You chose to open this file: " +
+		            chooser.getSelectedFile().getName());
+		       schemaFile=chooser.getSelectedFile();
+		    }
+					   
+		}
 		
 	}
 
