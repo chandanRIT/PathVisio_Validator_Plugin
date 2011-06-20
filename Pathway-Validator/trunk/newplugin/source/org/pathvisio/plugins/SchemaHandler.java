b3f3b3bfa94bea8627b3dbc0b168b60322d8b9d3
chandankmit <chandankmit@55a0e026-e783-40ef-8df5-1463b6136d98>
2011-06-20 17:46:44 +0000

    + code for the SchemaHandler added which eliminates the use of "whichSchema" and "ExtractPhaseValuesFromSchema" methods from the plugin. Now the schema file's search for title, phases , type of schema are done through the "parseSchemaAndSetValues" method instead

---
+++ Pathway-Validator/trunk/newplugin/source/org/pathvisio/plugins/SchemaHandler.java
@@ -0,0 +1,89 @@
+package org.pathvisio.plugins;
+
+import java.util.ArrayList;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SchemaHandler extends DefaultHandler{
+
+	 private String theTitle;	//schema's title
+	 private String type;	// schema's type
+	 private ArrayList<String> phases= new ArrayList<String>();
+	 private StringBuilder chars = new StringBuilder();
+	 private int iso_ns_counter=0;// <iso:ns> tag counter 
+	 
+	 public void startElement(String uri, String localName, String rawName, Attributes attributes) {
+		 
+		 if (rawName.equals("iso:ns")) {
+			 
+			 iso_ns_counter++;
+			 
+			 if(iso_ns_counter==1){
+				 this.type=attributes.getValue("prefix");
+		 	 }
+		 
+		 }
+		 
+		 else if (rawName.equals("iso:title")) { // here getcharacters() necessary to flushout the empty spaces
+			 this.theTitle=getCharacters();
+		 }
+		 
+		 else if (rawName.equals("iso:phase")){
+			 phases.add(attributes.getValue("id"));
+		 }
+		 
+		 
+			    
+		 
+	 }
+	 
+	 public void endElement(String namespaceURL, String localName, String rawName) {
+		 
+		  if (rawName.equals("iso:title")) {
+			 this.theTitle=getCharacters();
+		 }
+		 
+	 }
+
+	 public void characters(char[] ch, int start, int length) {
+		    // print svrl:text text node if the lastElement is svrl:text
+		    this.chars.append (ch, start, length);
+	 }
+
+	 private String getCharacters() {
+		    String retstr = this.chars.toString();
+		    this.chars.setLength(0);
+		    return retstr;
+	 }
+	
+	 
+	 // getters and setters are below
+	 
+	public void setPhases(ArrayList<String> phases) {
+		this.phases = phases;
+	}
+
+	public ArrayList<String> getPhases() {
+		return phases;
+	}
+
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setTheTitle(String theTitle) {
+		this.theTitle = theTitle;
+	}
+
+	public String getTheTitle() {
+		return theTitle;
+	}
+
+		
+}
