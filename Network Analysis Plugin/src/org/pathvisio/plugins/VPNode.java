c122a9db0b56f1556d9d061a1728d5e1b76730b1
chandankmit <chandankmit@55a0e026-e783-40ef-8df5-1463b6136d98>
2011-07-28 11:03:10 +0000

    + moved the NA plugin module to trunk from Pathway-Validator folder

---
+++ Network Analysis Plugin/src/org/pathvisio/plugins/VPNode.java
@@ -0,0 +1,70 @@
+package org.pathvisio.plugins;
+
+//ALL the Node and Edge related classes defined here
+
+class VPNode{
+	int id;// 
+	String graphId; // all the nodes will have this
+		
+}
+
+class VPDataNode extends VPNode{
+	static int count=0;
+	String dNodeType;
+	
+	public VPDataNode(String gid, String dnt){
+		graphId=gid;
+		dNodeType=dnt;
+		id=++count;
+	}
+	
+	public String toString(){
+		return "DN-"+id+" ("+graphId+")";
+	}
+}
+
+class VPLine extends VPNode{
+	static int count=0;// id assigned to each node based on its type
+	String[] graphRef=new String[2];// since 1 line always refers to 2 nodes (the 2 nodes being its end-points)
+	
+	public VPLine(){
+		id=++count;
+	}
+	
+	public String toString(){
+		return "L-"+id+" ("+graphId+")";
+	}
+}
+
+class VPAnchor extends VPNode{
+	static int count=0;
+	String shape;
+	VPNode connectedToLine;
+	
+	public VPAnchor(String shape, String gid, VPLine vpl){
+		this.shape=shape;
+		graphId=gid;
+		connectedToLine=vpl;
+		id=++count;
+	}
+	
+	public String toString(){
+		return "A-"+id+" ("+graphId+")";
+	}
+}
+
+class JPEdge{
+	String[] nodesConnectedTo= new String[2];
+	static int count=0;
+	int eId;
+	
+	public JPEdge(String nId1,String nId2){
+		nodesConnectedTo[0]=nId1;
+		nodesConnectedTo[1]=nId2;
+		eId=++count;
+	}
+	
+	public String toString(){
+		return "E-" + eId;
+	}
+}
