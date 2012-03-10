2b46a20b1389335deabaecec2791714f3190e921
martijn <martijn@55a0e026-e783-40ef-8df5-1463b6136d98>
2012-03-10 21:39:53 +0000

    OSGi-work: implemented activator

---
+++ Validator_Plugin/source/org/pathvisio/plugins/Activator.java
@@ -0,0 +1,24 @@
+package org.pathvisio.plugins;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.pathvisio.desktop.plugin.Plugin;
+
+public class Activator implements BundleActivator
+{
+	private Plugin plugin;
+	
+	@Override
+	public void start(BundleContext context) throws Exception
+	{
+		plugin = new ValidatorPlugin();
+		context.registerService(Plugin.class.getName(), plugin, null);
+	}
+
+	@Override
+	public void stop(BundleContext arg0) throws Exception
+	{
+		plugin.done();
+	}
+
+}
