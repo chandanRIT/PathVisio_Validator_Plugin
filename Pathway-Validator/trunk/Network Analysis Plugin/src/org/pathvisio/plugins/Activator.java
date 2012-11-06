f50fb554da6509df3bbfb059d45e25187b736abe
AugustinLuna <AugustinLuna@55a0e026-e783-40ef-8df5-1463b6136d98>
2012-11-06 06:07:23 +0000

    - Changed build.xml to add the MANIFEST.MF to the resulting jar. - Updated plugin ID and Name in MANIFEST.MF - Added a check for missing GraphIDs of lines and adds them before running network algorithms - Moved all code files into a single package org.pathvisio.plugins. - Minor formatting changes - Changed Activator.java to use org.pathvisio.desktop.plugin.Plugin.

--- Pathway-Validator/trunk/Network Analysis Plugin/src/org/pathvisio/plugins/Activator.java
+++ Pathway-Validator/trunk/Network Analysis Plugin/src/org/pathvisio/plugins/Activator.java
@@ -1,26 +1,22 @@
-package org.pathvisio.NWAplugin;
-
+package org.pathvisio.plugins;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.pathvisio.desktop.plugin.Plugin;
-import org.pathvisio.plugins.GraphAnalysisPlugin;
 
 public class Activator implements BundleActivator {
+
+	private Plugin plugin;
 	
 	@Override
 	public void start(BundleContext context) throws Exception {
-		GraphAnalysisPlugin plugin = new GraphAnalysisPlugin();
+		plugin = new GraphAnalysisPlugin();
 		context.registerService(Plugin.class.getName(), plugin, null);
-		
-		
-
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
-		// TODO Auto-generated method stub
-	 
+		plugin.done();	 
 	}
 
 }
